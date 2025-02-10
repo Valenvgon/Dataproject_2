c6a3d4e190398989482a5bbc0da45e8b3a6479f1
Marcanta7 <marcanta7@gmail.com>
2025-02-10 19:52:39 +0100

    TERRAFORM in progress

--- TERRAFORM/main.tf
+++ TERRAFORM/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 provider "google" {
     project = var.project_id
     region  = "europe-west1"
@@ -27,33 +26,4 @@ resource "google_storage_bucket" "dataflow_bucket" {
 resource "google_bigquery_dataset" "dataset" {
     dataset_id = "dataflow_dataset"
     location   = "EU"
-=======
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-module "data_generator" {
-  source       = "./modules/Data_generator"
-  project_id   = var.project_id
-  region       = var.region
-  artifact_repo = var.artifact_repo
-}
-
-module "dataflow" {
-  source          = "./modules/Dataflow"
-  project_id      = var.project_id
-  region          = var.region
-  dataflow_bucket = "${var.project_id}-dataflow-bucket"
->>>>>>> Stashed changes
 }
