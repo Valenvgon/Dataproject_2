6507a4bf8807d92760c42822837a805c8852d30b
Valenvgon <vvercherg@gmail.com>
2025-02-11 02:26:19 +0100

    ya etsa aqui el dataflow que macheaa
    un macho de dataflow jajajja, sorry dataflow mejor hecho y queda pulir el camino de los mensajes. pero va por buen camino, mañana lo termino

--- 01_scripts_mejor_hechos/dataflow_pipeline.py
+++ 01_scripts_mejor_hechos/dataflow_pipeline.py
@@ -17,50 +17,41 @@ import json
 beam.options.pipeline_options.PipelineOptions.allow_non_parallel_instruction_output = True
 DataflowRunner.__test__ = False
 
+
 def ParsePubSubMessages(message): 
     pubsub_message= message.decode('utf-8')
 
     msg = json.loads(pubsub_message)
 
-    logging.info("New message: %s", msg)
+    #logging.info("New message: %s", msg)
 
-    return msg['city'], msg
+    return msg
 
-class BusinessLogicDoFn(beam.DoFn):
-    def process(self, element):
+class MatchVoluntariosAfectados(beam.DoFn):
 
-        city, grouped_data = element
-        affected_list = grouped_data.get('affected', []) if isinstance(grouped_data, dict) else []
-        volunteer_list = grouped_data.get('volunteer', []) if isinstance(grouped_data, dict) else []
-        
-        matched_data = []
-        unmatched_data = []
-        
-        for affected in affected_list:
-            matched = False
-            for volunteer in volunteer_list:
-                if (affected['necessity'] == volunteer['necessity'] and
-                    affected['disponibility'] == volunteer['disponibility'] and
-                    affected['city'] == volunteer['city']):
-                    matched_data.append({
-                        "affected": affected,
-                        "volunteer": volunteer,
-                        "matched_timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-                    })
-                    matched = True
-                    break
-            if not matched:
-                unmatched_data.append(affected)
-        
-        for volunteer in volunteer_list:
-            if all(volunteer != match['volunteer'] for match in matched_data):
-                unmatched_data.append(volunteer)
-        
-        yield beam.pvalue.TaggedOutput("matched_data", matched_data)
-        yield beam.pvalue.TaggedOutput("unmatched_data", unmatched_data)
+    def process(self, affected_data, volunteer_data):
 
+        import apache_beam as beam
 
+        city = affected_data["city"]
+        necessity = affected_data["necessity"]
+        disponibility = affected_data["disponibility"]
 
+        volunteer_filtered = [
+            v for v in volunteer_data
+            if v["city"] == city and necessity == v["necessity"] and disponibility == v["disponibility"]
+        ]
+
+        if volunteer_filtered:
+            match = {
+                "afectado": affected_data,
+                "voluntario": volunteer_filtered[0]
+            }
+            logging.info(f"MATCH encontrado: {match}")
+            yield beam.pvalue.TaggedOutput('matched_data', match)
+        else:
+            logging.info(f"NO MATCH para afectado: {affected_data}")
+            yield beam.pvalue.TaggedOutput('non_matched_data', affected_data)
 
 
 def run(): 
@@ -113,20 +104,24 @@ def run():
                 |"Fixed window for Volunteer data" >> beam.WindowInto(beam.window.FixedWindows(90))
         )
 
-        # co Group by key 
-        grouped_data= (
-            affected_data, volunteer_data) | "merge PCollection" >> beam.CoGroupByKey()
-
-        processed_data= (grouped_data
-            |"Check the matched messages" >> beam.ParDo(BusinessLogicDoFn()).with_outputs("matched_data", "unmatched_data"))
-        
-        send_data= (
-            
+        results = (
+            affected_data 
+            | "Buscar Matches" >> beam.ParDo(MatchVoluntariosAfectados(), beam.pvalue.AsList(volunteer_data))
+            .with_outputs('matched_data', 'non_matched_data')
         )
 
-        send_data.matched_data | "Publish matched messages" >> beam.io.WriteToPubSub(args.output_topic_matched)
+        matches = results['matched_data'] 
+        sin_match = results['non_matched_data']
 
-        send_data.unmatched_data | "Publish unmatched messages" >> beam.io.WriteToPubSub(args.output_topic_non_matched)
+        # Enviar Matches a Pub/Sub
+        matches | "Formato Matches JSON" >> beam.Map(lambda x: json.dumps(x).encode("utf-8")) \
+                | "Enviar Matches a Pub/Sub" >> beam.io.WriteToPubSub(topic=args.output_topic_matched)
+
+        # Enviar No Matches a Pub/Sub
+        sin_match | "Formato No-Matches JSON" >> beam.Map(lambda x: json.dumps(x).encode("utf-8")) \
+                  | "Enviar No-Match a Pub/Sub" >> beam.io.WriteToPubSub(topic= args.output_topic_non_matched)
+
+        
 
 if __name__ == '__main__':
 
@@ -154,12 +149,27 @@ Till here i want to prove if the code is correct let's run it on dataflow  run p
     --output_topic_matched 'projects/data-project-2425/topics/matched' \
     --system_id 'vvercherg' \
     --runner DataflowRunner \
-    --job_name 'data-flow-pruebas-3' \
+    --job_name 'data-flow-pruebas-999' \
     --region 'europe-west1' \
     --temp_location 'gs://dataflow_bucket_dataproject_2425/tmp' \
     --staging_location 'gs://dataflow_bucket_dataproject_2425/stg' \
     --requirements_file 'requirements.txt'
 
+    python dataflow_pipeline.py \
+    --project_id 'data-project-2425' \
+    --affected_sub 'projects/data-project-2425/subscriptions/affected-sub' \
+    --volunteer_sub 'projects/data-project-2425/subscriptions/volunteer-sub' \
+    --output_topic_non_matched 'projects/data-project-2425/topics/no-matched' \
+    --output_topic_matched 'projects/data-project-2425/topics/matched' 
+    
+
+
+    topics :
+
+    affected 
+    volunteer
+    matched
+    no-matched 
     
     de aqui sacamos en claro que lee los mensajes y que le hace la window de 90 segundos
         '''
